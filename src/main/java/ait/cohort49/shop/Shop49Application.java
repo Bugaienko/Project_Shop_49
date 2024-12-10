0ef447aaa6cffec419de692da8e9e6b94a76e64e
SergeyB <sergiibugaienko@gmail.com>
2024-12-10 09:18:15 +0100

    init commit

---
+++ src/main/java/ait/cohort49/shop/Shop49Application.java
@@ -0,0 +1,13 @@
+package ait.cohort49.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop49Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shop49Application.class, args);
+    }
+
+}
---
+++ src/test/java/ait/cohort49/shop/Shop49ApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.cohort49.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop49ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
