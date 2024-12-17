4555b7018aeceadba62fb70273ef114038945e92
SergeyB <sergiibugaienko@gmail.com>
2024-12-17 13:19:20 +0100

    JWT add

---
+++ src/main/java/ait/cohort49/shop/security/dto/LoginRequestDTO.java
@@ -0,0 +1,14 @@
+package ait.cohort49.shop.security.dto;
+
+import java.util.Objects;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 17.12.2024
+ */
+
+
+public record LoginRequestDTO(String username, String password) {
+
+
+}
---
+++ src/main/java/ait/cohort49/shop/security/dto/RefreshRequestDTO.java
@@ -0,0 +1,37 @@
+package ait.cohort49.shop.security.dto;
+
+import java.util.Objects;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 16.12.2024
+ */
+
+public class RefreshRequestDTO {
+    private String refreshToken;
+
+    @Override
+    public String toString() {
+        return String.format("Refresh request: refreshToken: %s", refreshToken);
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof RefreshRequestDTO that)) return false;
+
+        return Objects.equals(refreshToken, that.refreshToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(refreshToken);
+    }
+}
